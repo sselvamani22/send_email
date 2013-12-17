0fe105313671611e25c0ce41d7420ce0cbefd407
sselva <sselvamani22@gmail.com>
2013-12-17 17:15:28 +0530

    added codes to project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mails::Application.config.secret_token = '1b22bab029d361b39b3ee1049bc056ff3378a4a7b024f31288eca4bb636f5f9d21edfdf4e8624ad41b495177026986dd7a2b6d8289902daaf98cbb118777d97e'
